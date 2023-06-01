6f38b7bace4ea1c48508d93dc43168259fa3a281
Lynette <lynettecargo@Lynettes-Air.home>
2023-06-01 14:32:54 -0500

    Restaurant MockUp Lab

---
+++ chilis.js
@@ -0,0 +1,59 @@
+let menu = {
+    appetizers: [
+      "Texas Cheese Fries",
+      "Buffalo Wings",
+      "Skillet Queso",
+      "Fried Mozerella",
+    ],
+
+    entrees: [
+        "Ancho Salmon",
+        "Santa Fe Salad",
+        "Classic Sirloin",
+        "Babyback Ribs - Full Rack",
+        "Babyback Ribs - Half Rack",
+        "Ultimate Cajan Pasta",
+      ],
+
+      dessert: ["Skillet Chocolate Chip Cookie", "Molton Lava Cake", "Cheesecake"],
+
+      kidsMenu: [
+        "Burger Bites",
+        "Chicken Bites",
+        "Cheese Quesidilla",
+        "Chicken Tenders",
+        "Mac & Cheese",
+      ],
+
+      beverages: [
+        "Coca",
+        "Diet Coke",
+        "Coke Zero",
+        "Spite",
+        "Dr. Pepper",
+        "MinuteMaid Lemonade",
+        "Sweet Tea",
+        "Strawberry Lemonade",
+        "Waermelon Lemonade",
+        "Watermelon Tea",
+        "Blackberry Iced Tea",
+        "Mango Iced Tea",
+      ],
+    };
+
+    let lunchSpecials = [
+        "Big Mout Cripsy Chicken Sandwhich",
+        "Chicken Bacon Rnach Quesidilla - Half Order",
+        "Double Burger",
+        "Bacon Avacado Grilled Chicken",
+        "Chipotle Chicken Fresh Mex Bowl",
+      ];
+
+      let spirits = [
+        "Presidente Maragrita",
+        "Tiki Party Punch Rita",
+        "Frozen Margarita",
+        "Tito's Punch",
+        "Captain's Castaway",
+        "Texas Strawberry Lemonade",
+      ];
